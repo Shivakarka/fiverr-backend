37bd76a9228f4be1fc27645aee437b59cd99e457
shivakarka <shivakarka@gmail.com>
2023-11-12 23:26:49 +0530

    Add logout functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,6 +48,11 @@ export const login = async (req, res, next) => {
 };
 
 export const logout = async (req, res) => {
-  try {
-  } catch (error) {}
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
 };
