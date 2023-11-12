519ff9571c90227c27959a499235b65e26dfcf1d
shivakarka <shivakarka@gmail.com>
2023-11-12 22:41:03 +0530

    Add JWT verification middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { deleteUser } from "../controllers/userController.js";
+import { verifyToken } from "../middlewares/verifyJWT.js";
 
 const router = express.Router();
 
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
