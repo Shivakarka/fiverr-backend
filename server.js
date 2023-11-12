1b96e242bcb5baf026a03770175d81a7a5371cc6
shivakarka <shivakarka@gmail.com>
2023-11-12 19:44:03 +0530

    Added dotenv , mongoose &  connection to mongoDb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import connectDB from "./config/dbConfig.js";
 
 const app = express();
+
+connectDB();
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
