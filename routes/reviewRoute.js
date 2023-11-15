7b8903a0528d53c1ae94dafc25d344a02b4a4c66
shivakarka <shivakarka@gmail.com>
2023-11-15 09:18:31 +0530

    Add review functionality to the app

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { verifyToken } from "../middlewares/verifyJWT.js";
+import {
+  createReview,
+  deleteReview,
+  getReviews,
+} from "../controllers/reviewController.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send("Hello World!");
-});
+router.post("/", verifyToken, createReview);
+router.get("/:gigId", getReviews);
+router.delete("/:id", deleteReview);
 
 export default router;
